9eba845f5ca75ce49909a7c0addafa4b5e8cbc86
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-02-01 15:36:27 +0100

    decorated models and added migration

--- workshop.wwwapi/Models/Appointment.cs
+++ workshop.wwwapi/Models/Appointment.cs
@@ -1,13 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace workshop.wwwapi.Models
 {
     //TODO: decorate class/columns accordingly
+    [PrimaryKey(nameof(DoctorId), nameof(PatientId))]
     public class Appointment
     {
-        
+        [Column("booking")]
         public DateTime Booking { get; set; }
+
+        [ForeignKey("doctor_id")]
+        [Column("doctor_id")]
         public int DoctorId { get; set; }
+
+        [ForeignKey("patient_id")]
+        [Column("patient_id")]
         public int PatientId { get; set; }
 
     }
--- workshop.wwwapi/Models/Patient.cs
+++ workshop.wwwapi/Models/Patient.cs
@@ -1,12 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 
 namespace workshop.wwwapi.Models
 {
     //TODO: decorate class/columns accordingly    
+    [PrimaryKey("Id")]
     public class Patient
-    {        
-        public int Id { get; set; }        
-        public string FullName { get; set; }
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
     }
 }
