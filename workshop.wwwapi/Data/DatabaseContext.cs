ccf998a8c95deafd07f94eb85a1f401fef667d6d
Julia <julia.r.lindgren@gmail.com>
2024-09-06 10:04:00 +0200

    Complete part 1 setup

--- workshop.wwwapi/Data/DatabaseContext.cs
+++ workshop.wwwapi/Data/DatabaseContext.cs
@@ -12,14 +12,27 @@ namespace workshop.wwwapi.Data
         {
             var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             _connectionString = configuration.GetValue<string>("ConnectionStrings:DefaultConnectionString")!;
-            this.Database.EnsureCreated();
+            //this.Database.EnsureCreated();
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //TODO: Appointment Key etc.. Add Here
-            
 
             //TODO: Seed Data Here
+            modelBuilder.Entity<Patient>().HasData(
+               new Patient
+               {
+                   Id = 1,
+                   FullName = "John Doe"
+               },
+               new Patient
+               {
+                   Id = 2,
+                   FullName = "Jane Smith"
+               }
+            );
+
+            base.OnModelCreating(modelBuilder);
 
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
--- workshop.wwwapi/Models/Appointment.cs
+++ workshop.wwwapi/Models/Appointment.cs
@@ -1,13 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace workshop.wwwapi.Models
 {
     //TODO: decorate class/columns accordingly
+    [Table("appointments")]
     public class Appointment
     {
-        
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("booking")]
         public DateTime Booking { get; set; }
+        [Column("doctor_id")]
         public int DoctorId { get; set; }
+        [Column("patient_id")]
         public int PatientId { get; set; }
 
     }
