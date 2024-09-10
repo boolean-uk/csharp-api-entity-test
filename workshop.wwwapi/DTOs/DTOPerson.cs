d90ff2b2b03d32dc25ed13102bcc589a5fe0e57c
Victor <victor.chicinas@gmail.com>
2024-09-10 10:35:04 +0200

    Core done including Part 3. Tests remaining.

---
+++ workshop.wwwapi/DTOs/DTOPerson.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using workshop.wwwapi.Models;
+
+namespace workshop.wwwapi.DTOs
+{
+    public class DTOPerson
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+    }
+}
--- workshop.wwwapi/Models/Doctor.cs
+++ workshop.wwwapi/Models/Doctor.cs
@@ -1,11 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace workshop.wwwapi.Models
 {
     //TODO: decorate class/columns accordingly    
+    [Table("Doctors")]
     public class Doctor
-    {        
-        public int Id { get; set; }        
+    {
+        [Required]
+        [Column("Id", TypeName = "int")]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("FullName")]
         public string FullName { get; set; }
+
+        [Column("Appointments")]
+        public ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
     }
 }
