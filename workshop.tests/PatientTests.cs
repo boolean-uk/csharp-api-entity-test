7b2b47ea34104647233ec4eaaa7bd2c1fba83c24
Thomas Aleksander Flier <thomas.flier@no.experis.com>
2024-09-11 12:07:38 +0200

    tests done

--- workshop.tests/PatientTests.cs
+++ workshop.tests/PatientTests.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using System.Text;
+using workshop.wwwapi.DTOs;
+using workshop.wwwapi.Models;
 
 namespace workshop.tests;
 
@@ -17,6 +21,97 @@ public class Tests
         var response = await client.GetAsync("/patients");
 
         // Assert
-        //Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.OK);
+        Assert.That(response.StatusCode == System.Net.HttpStatusCode.OK);
+    }
+
+    [Test]
+    public async Task PatientCheck()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/patients");
+        var json = await response.Content.ReadAsStringAsync();
+        var patients = JsonConvert.DeserializeObject<IEnumerable<PatientDTO>>(json);
+
+        var patient = patients.First();
+        // Assert
+        Assert.That(patient.Name, Is.EqualTo("Dennis"));
+    }
+
+    [Test]
+    public async Task CreatePatientCheck()
+    {
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        PatientDTO input = new PatientDTO() { Name = "Thomas Flier" };
+        var json = new StringContent(JsonConvert.SerializeObject(input), UTF8Encoding.UTF8, "application/json");
+
+        var response = await client.PostAsync("/patients", json);
+
+        Assert.That(response.StatusCode == System.Net.HttpStatusCode.OK);
+    }
+
+    [Test]
+    public async Task DoctorEndpointStatus()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/doctors");
+
+        // Assert
+        Assert.That(response.StatusCode == System.Net.HttpStatusCode.OK);
+    }
+
+    [Test]
+    public async Task DoctorCheck()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/doctors");
+        var json = await response.Content.ReadAsStringAsync();
+        var patients = JsonConvert.DeserializeObject<IEnumerable<PatientDTO>>(json);
+
+        var patient = patients.First();
+        // Assert
+        Assert.That(patient.Name, Is.EqualTo("Doctor Osmani"));
+    }
+
+    [Test]
+    public async Task CreateDoctorCheck()
+    {
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        PatientDTO input = new PatientDTO() { Name = "Doctor Flier" };
+        var json = new StringContent(JsonConvert.SerializeObject(input), UTF8Encoding.UTF8, "application/json");
+
+        var response = await client.PostAsync("/doctors", json);
+
+        Assert.That(response.StatusCode == System.Net.HttpStatusCode.OK);
+    }
+
+
+    [Test]
+    public async Task AppointmentEndpointStatus()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync("/appointments");
+
+        // Assert
+        Assert.That(response.StatusCode == System.Net.HttpStatusCode.OK);
     }
 }
